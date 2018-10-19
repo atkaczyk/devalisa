12128194ca45ac87877c198df22a4311da722054
atkaczyk <alisa.tk@gmail.com>
2018-10-18 22:28:11 -0400

    favicon added

--- website/src/App.js
+++ website/src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+
+      return (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+
+        <NavBar />
+  </MuiThemeProvider>
+);
+      }
+    }
 
 export default App;
