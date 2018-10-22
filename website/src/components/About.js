0acdfba2cf0810bc76f5c8d31074de3bd50ec7ab
atkaczyk <alisa.tk@gmail.com>
2018-10-22 17:15:11 -0400

    modified about component to contain info about me

--- website/src/components/About.js
+++ website/src/components/About.js
@@ -1,54 +1,94 @@
-import React, { Component } from 'react';
-
-
+import React from 'react';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
-import Image from '../img/landing-page.jpg'; // Import using relative path
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
-const styles = {
-    aboutContainer: {
-        backgroundImage: `url(${Image})`,
-        height: '100%',
-        height: '700px',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'scroll',
-        backgroundPosition: 'initial',
-        backgroundSize: 'cover',
-        alignText: 'center',
-        paddingTop:'3%'
-    },
-    introHeading: {
+const styles = theme => ({
+  root: {
+    overflow: 'hidden',
+    padding: `0 ${theme.spacing.unit * 3}px`,
+  },
+  wrapper: {
+      width:'50%'
+  },
+  paper: {
+    margin: theme.spacing.unit,
+    padding: theme.spacing.unit * 2,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
 
-    }
-};
+function AutoGridNoWrap(props) {
+  const { classes } = props;
+  const message = `Truncation should be conditionally applicable on this long line of text
+                    as this is a much longer line than what the container can support. `;
 
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Paper className={classes.paper}>
+          <Grid container wrap="nowrap" spacing={16}>
+            <Grid item>
+              <Avatar>Me</Avatar>
+            </Grid>
+            <Grid item xs>
 
-function About (props) {
-
-    const { classes } = props;
-
-        return (
-            <div id="about" className="row" className={ classes.aboutContainer  }>
-                <div className='intro-heading'>Alisa Tkaczyk</div>
-                <div className="intro-lead-in">Software Developer.</div>
-                <div className="row about-icon-wrapper">
-                    <a href="https://ca.linkedin.com/in/alisa-tkaczyk-94085683"  rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-linkedin-square fa-5x" aria-hidden="true" />
-                    </a>
-                    <a href="https://www.google.ca/search?dcr=0&amp;source=hp&amp;ei=xjQKWvKqOqjujwS_9p3wAg&amp;q=alisa+tkaczyk&amp;oq=alisa+&amp;gs_l=psy-ab.3.0.35i39k1j0i10k1j0l4j0i10k1j0l3.2118.3016.0.3706.9.7.1.0.0.0.183.762.2j4.6.0....0...1.1.64.psy-ab..2.7.770.0..0i131k1.0.WyguLE2aHQM"  rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-user fa-5x" aria-hidden="false" />
-                    </a>
-                    <a href="https://github.com/atkaczyk" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-github-square fa-5x" aria-hidden="true" />
-                    </a>
+                <div className={classes.heroContent}>
+                    <Typography component="h3" variant="h3" align="left" color="textPrimary" gutterBottom>
+                        About</Typography>
+                    <Typography variant="h6" align="left" color="textSecondary" paragraph>
+                      Hi! I'm A-Lisa. </Typography>
                 </div>
-            </div>
-        );
+                <div className={classes.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>My past</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>I studied Honours Computer Science at Carleton University, where I specialized in Psychology and Human Computer Interaction. There I was also a Varsity Rower and a member of the Carleton Computer Science Society Board of Directors. I worked part-time at Real Programming 4 Kids teaching youth how to build usable video game projects, I worked part time at the school cafe as well. My favourite subjects were AI, Cryptography, and HCI. My biggest accomplishment was publishing a paper.</Typography>
+
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>My present</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>I began working as a Software Consultant for Tata Consultancy Services in January 2018. While I was here I worked on multiple projects for one of the five major Canadian banks. My favourite project was working on the marketing team for the banks public site, we rolled out multiple campaigns from end to end and followed closely with agile. My biggest accomplishment was exceeding client expectations as I built a POC and proposed a solution to the companies onboarding difficulties I was then awarded Star of the Quarter award to recognize and employee who is innovative and self-starting.</Typography>
+              
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>My Future</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+        <Typography>In my free time I enjoy being active. I coach youth basketball, I volunteer for Ladies Learning Code, I go for walks around the city. I also enjoy being creative. I take pottery and sewing classes, I also do punch-needle embroidery and paint.</Typography>
+                  </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    </div>
+  );
 }
 
-About.propTypes = {
-    classes: PropTypes.object.isRequired,
+AutoGridNoWrap.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(About);
+export default withStyles(styles)(AutoGridNoWrap);
+
